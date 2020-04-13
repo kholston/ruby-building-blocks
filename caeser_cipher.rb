02b340ab73b9cad252f72f29dcb9a3ab39e79cb5
Ken Holston <git.kholston@gmail.com>
2020-04-13 13:05:38 -0400

    Add program description and def block

---
+++ caeser_cipher.rb
@@ -0,0 +1,11 @@
+# This program implements a simple encryption technique
+# called Caesar Cipher. It is a type of substitution 
+# cipher in which each letter is replaced by a letter
+# some fixed number positions down the alphabet.
+
+
+def ceasar_cipher(string, number)
+
+
+    
+end
