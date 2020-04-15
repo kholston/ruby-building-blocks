03860bb6b6fc7aceeadbbf767edfe35f0dc6595c
Ken Holston <git.kholston@gmail.com>
2020-04-15 14:12:54 -0400

    Finish caesar cipher

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -4,8 +4,23 @@
 # some fixed number positions down the alphabet.
 
 
-def ceasar_cipher(string, number)
+def ceasar_cipher(input, shifter)
 
+    alphabet = Array('a'..'z')
+    lower_case = Hash[alphabet.zip(alphabet.rotate(shifter))]
 
+    alphabet = Array('A'..'Z')
+    upper_case = Hash[alphabet.zip(alphabet.rotate(shifter))]
+
+    cipher = lower_case.merge(upper_case)
+
+    input.chars.map{ |c| cipher.fetch(c , c)}.join
     
 end
+
+puts "Enter something to encrypt: "
+string = gets.chomp
+puts "Enter the amount to shift it: "
+shift = gets.chomp
+answer = ceasar_cipher(string,shift.to_i)
+puts "Your encryption equals #{answer}"
