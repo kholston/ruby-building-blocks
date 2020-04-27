abc6503896f7e732ba8504b0568a3ed0d66d0cef
Ken Holston <git.kholston@gmail.com>
2020-04-26 23:47:12 -0400

    Complete bubble_sort problem

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,16 @@
 def bubble_sort(input)
- 
+    max_index = input.length
+    until max_index < 1
+        new_max = 0
+        for i in 1..max_index - 1 do
+            if input[i - 1] > input[i]
+                input[i - 1], input[i] = input[i] , input[i - 1]
+                new_max = i
+            end 
+        end 
+        max_index = new_max       
+    end
+    puts "#{input}"
 end
 
 bubble_sort([4,3,78,2,0,2])
